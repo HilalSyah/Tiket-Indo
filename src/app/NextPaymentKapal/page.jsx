c2b0ba6a3684ed07ff3cfd506c19c530b62eebab
HilalSyah <159630685+HilalSyah@users.noreply.github.com>
2024-09-25 16:02:30 +0700

    Update Eror

--- src/app/NextPaymentKapal/page.jsx
+++ src/app/NextPaymentKapal/page.jsx
@@ -28,7 +28,7 @@ export default function Home() {
         <main className="max-w-xl mx-auto sm:p-16">
           <div className="chek border-[2px] h-[35rem] border-slate-400 rounded-[1rem] shadow-md shadow-slate-600 bg-white">
             <div className="flex flex-col">
-              <Image src="/Background (11).png" alt="Kapal Gambar" width={250} height={250} objectFit="cover" className="w-full rounded-t-[1rem]" />
+              <Image src="/kapal.png" alt="Kapal Gambar" width={250} height={250} objectFit="cover" className="w-full rounded-t-[1rem]" />
               <div className="border border-gray-100 bg-white p-6">
                 <h3 className="mt-4 text-lg font-medium text-gray-900">{productKapal.name}</h3>
                 <p className="mt-1.5 text-sm text-gray-700">Rp {productKapal.price}</p>
